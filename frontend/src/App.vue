b3159779ba0043ab659d8d7565098bc9f4ddd330
FreeFly0125 <135254388+FreeFly0125@users.noreply.github.com>
2024-06-27 12:58:59 +0900

    [FEAT]Design UI

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+/* Add your styles here */
+</style>
--- frontend/src/components/common/Spinner.vue
+++ frontend/src/components/common/Spinner.vue
@@ -7,9 +7,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
 export default defineComponent({
   name: 'LoadingSpinner',
   props: {
